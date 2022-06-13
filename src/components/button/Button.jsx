f2362d3b8b12cac85ea99caa8d2edad61dd17236
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2022-06-13 18:36:19 +0200

    init setup

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// SAMPLE BUTTON
+const Button = () => {
+  return (
+    <button className="button">
+      <span className="button__text">
+        Button
+      </span>
+    </button>
+  );
+}
+
+export default Button;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ** React 17.0.2
+import React from "react";
+import reactDom from "react-dom";
+import App from "./src/App"
+reactDom.render(<App />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// ** React 18.1.0
+/* import { createRoot } from 'react-dom/client';
+import React from "react";
+import App from "./src/App"
+const container = document.getElementById('app');
+const root = createRoot(container); 
+root.render(<App tab="home" />); */
